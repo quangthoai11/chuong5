20ad23d3d70cc1c82988d9285f457e6fc4374995
TranThanhNhat113 <nhattran14925@gmail.com>
2024-09-15 01:36:06 +0700

    Update level1.3.py

--- level1.3.py
+++ level1.3.py
@@ -4,10 +4,9 @@ Created on Sat Sep 14 16:10:18 2024
 
 @author: Cao Ngọc Thanh Thư 23719291
 """
-def ve_hinh(hang, cot):
-  for i in range(hang):
+hang = 8
+cot = 8
+for i in range(hang):
     for j in range(cot):
-      print("#", end="")
+        print("#", end=" ")
     print()
-
-ve_hinh(8, 8)
