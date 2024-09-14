27f13113e4418f2238949527672c256383fae981
TranThanhNhat113 <nhattran14925@gmail.com>
2024-09-15 01:20:45 +0700

    Update LAB01-46.py

--- LAB01-46.py
+++ LAB01-46.py
@@ -5,24 +5,14 @@ Created on Sat Sep 14 23:59:19 2024
 @author: Admin
 """
 
-def liet_ke_nghiem_toi_uu(a, b, c, n):
-  """Liệt kê tất cả các bộ nghiệm nguyên dương của phương trình ax + by + cz = n.
-
-  Args:
-    a, b, c: Hệ số của x, y, z.
-    n: Giá trị tự do.
-  """
-
-  for x in range(1, n // a + 1):
-    for y in range(1, (n - a*x) // b + 1):
-      z = n - a*x - b*y
-      if z % c == 0 and z > 0:
-        print(f"Nghiệm: (x, y, z) = ({x}, {y}, {z // c})")
-
-# Thay đổi các hệ số và giá trị tự do ở đây
-a = 2
-b = 7
-c = 9
-n = 979
-
-liet_ke_nghiem_toi_uu(a, b, c, n)
+a, b, c, n = 2, 7, 9, 979
+solutions = []
+for x in range(1, n // a + 1):
+    for y in range(1, (n - a * x) // b + 1):
+        if (n - a * x - b * y) % c == 0:
+            z = (n - a * x - b * y) // c
+            if z > 0:
+                solutions += [(x, y, z)]
+print("Các bộ nghiệm nguyên của phương trình là:")
+for sol in solutions:
+    print(sol)
