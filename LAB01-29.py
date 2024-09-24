76e3eb84874edf44c2a68c344f4df1a450a26651
TranThanhNhat113 <nhattran14925@gmail.com>
2024-09-24 22:52:01 +0700

    Update LAB01-29.py

--- LAB01-29.py
+++ LAB01-29.py
@@ -6,10 +6,8 @@ Created on Sat Sep 14 23:58:45 2024
 """
 
 N = int(input("Nhập vào một số nguyên dương N: "))
-while N <= 0:
-    print("N phải là số nguyên dương. Vui lòng nhập lại!")
-    N = int(input("Nhập vào một số nguyên dương N: "))
 tong = 0
-for i in str(N):
-    tong += int(i)
-print(f"Tổng các chữ số của {N} là: {tong}")
+while N > 0:
+    tong += N % 10
+    N //= 10
+print("Tổng các chữ số là:", tong)
