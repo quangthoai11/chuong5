a6a80da217d4a0b8f502da77dfab338b0022336d
quangthoai11 <quangthoaitrinhh@gmail.com>
2024-09-24 23:08:33 +0700

    Update LAB01-28.py

--- LAB01-28.py
+++ LAB01-28.py
@@ -7,9 +7,7 @@ Created on Sat Sep 14 23:37:04 2024
 
 N = int(input("Nhập vào một số nguyên dương N: "))
 while N <= 0:
-    print("N phải là số nguyên dương. Vui lòng nhập lại!")
     N = int(input("Nhập vào một số nguyên dương N: "))
-print(f"Các ước số của {N} là:")
 for i in range(1, N + 1):
     if N % i == 0:
         print(i)
